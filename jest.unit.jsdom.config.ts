e2d99b0b3f1833e403a506e84d9a9de04db9dd2d
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-10-24 16:16:59 +0200

    test unit test component and fn

---
+++ jest.unit.jsdom.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "/^@/(.*)$/": "C:Git\\testing-jest-app$1",
+  },
+  resolver: undefined,
+};
+
+export default createJestConfig(config);
