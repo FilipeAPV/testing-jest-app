b9dea90a1d0d759dfae81ba0d520f127f9fab350
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-10-25 08:03:52 +0200

    add integration testing to pipeline

---
+++ jest.integration.node.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/src/tests/integration/**/*.test.ts"],
+  moduleNameMapper: {
+    "/^@/(.*)$/": "C:Git\\testing-jest-app$1",
+  },
+  resolver: undefined,
+};
+
+export default createJestConfig(config);
